9576ca2d56ef9b7fc2f4001b86a3b1647325a984
Rakib Hossain ii <tanjibrakib80@gmail.com>
2024-03-02 22:14:37 +0600

    add web side

--- src/components/PrivetRoute.js
+++ src/components/PrivetRoute.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { Navigate, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { UseAuthor } from "../contexts/AuthorContext";
 
 function PrivateRoute({ element: Element, ...rest }) {
   const { currentUser } = UseAuthor();
 
   return (
-    <Route
-      {...rest}
-      element={currentUser ? <Element /> : <Navigate to="/login" replace />}
-    />
+    <>
+      {currentUser ? <Element {...rest} /> : <Navigate to="/login" replace />}
+    </>
+    // <Route
+    //   {...rest}
+    //   element={currentUser ? <Element /> : <Navigate to="/login" replace />}
+    // />
   );
 }
 
--- src/fire-base.js
+++ src/fire-base.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// import { addDoc, collection, getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,3 +17,33 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// // initalize service
+
+// const db = getFirestore();
+
+// const colRef = collection(db, "cars");
+// //function to add a car document to firestore
+
+// const addDealer = async (dealer) => {
+//   try {
+//     const decRef = await addDoc(collection(db, "dealers"), dealer);
+//     console.log("document written eith in", decRef.id);
+//   } catch (error) {
+//     console.error("errror adding document:", error);
+//   }
+// };
+
+// //fatch the fson file
+
+// fetch("http://localhost:3002/database")
+//   .then((response) => response.json())
+//   .then((dealerData) => {
+//     dealerData.forEach((dealer) => {
+//       addDealer(dealer);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("error ferching dealer data", error);
+//   });
+
+// export default app;
